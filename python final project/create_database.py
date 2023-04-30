c46b040941939ab23237265e17586e5686ea7af3
avery-weidner <129922746+avery-weidner@users.noreply.github.com>
2023-04-30 17:58:39 -0400

    added database table for images and made homepage nicer

--- python final project/create_database.py
+++ python final project/create_database.py
@@ -4,13 +4,34 @@ conn = sqlite3.connect('finalproject.db')
 
 c = conn.cursor()
 
-c.execute('''CREATE TABLE dogs
-             (name text, dateofbirth date)''')
+c.execute('drop table if exists dogs')
+c.execute('drop table if exists pictures')
 
-c.execute("INSERT INTO dogs VALUES ('nugget','12/1/2011')")
-c.execute("INSERT INTO dogs VALUES ('dixie','2/15/2022')")
-c.execute("INSERT INTO dogs VALUES ('rocco','2/9/2023')")
+c.execute('''CREATE TABLE dogs
+             (
+             	name text, 
+             	dateofbirth date,
+             	icon_filename text
+             )
+             ''')
+
+c.execute("INSERT INTO dogs VALUES ('nugget','12/1/2011','IMG_1693.png')")
+c.execute("INSERT INTO dogs VALUES ('dixie','2/15/2022','IMG_1692.png')")
+c.execute("INSERT INTO dogs VALUES ('rocco','2/9/2023','baddog_icon.jpg')")
+
+c.execute('''CREATE TABLE pictures
+             (dog text,
+              descr text,
+              filename text
+             )''')
+
+c.execute("INSERT INTO pictures VALUES ('nugget','nuggets birthday','nugget1.jpg')")
+c.execute("INSERT INTO pictures VALUES ('nugget','nuggets with grandma','nugget2.jpg')")
+#c.execute("INSERT INTO pictures VALUES ('nugget','nugget','IMG_1693.png')")
+c.execute("INSERT INTO pictures VALUES ('dixie','dixie being fresh','IMG_1692.PNG')")
+
 
 conn.commit()
 
 conn.close()
+
--- python final project/main.py
+++ python final project/main.py
@@ -5,21 +5,64 @@ import sqlite3
 
 app = Flask(__name__)
 
+def table_cell(cell_text):
+    return "<td align=center>"+cell_text+"</td>"
 
+def table_row(row_text):
+    return "<tr>"+row_text+"</tr>"
+
+def htmltable(rows,parameters):
+    return "<table "+parameters+">"+rows+"</table>"
+
+def dog_info(dogname,icon_filename):
+    return "<a href=\""+dogname+"\"> <img src=images/"+icon_filename+" width=100><br>"+dogname+" </a>"
+
+#default page
 @app.route("/")
 def index():
     conn = sqlite3.connect('finalproject.db')
     c = conn.cursor()
-    HTML="My dogs are:<br>"
-    for row in c.execute('SELECT * FROM dogs ORDER BY dateofbirth'):
-        dogname=row[0]
-        HTML+= "<li><a href=\""+dogname+"\">"+dogname+"</a>"
-    return HTML
+    HTML='''<head><title>Avery's dog friends</title></head>
+        <body>
+        <center><h1>Avery's dog friends</h1>
 
+        Welcome to my website. I chose to make an interactive photo album for my python class.
+
+        <h4>Click on the dog's name to see photos</h4>
+        </center>
+    '''
+    cells=""
+    for row in c.execute('SELECT name,icon_filename FROM dogs ORDER BY dateofbirth'):
+        dogname=row[0]
+        icon_filename=row[1]
+        cells+= table_cell(dog_info(dogname,icon_filename))
+
+    HTML+=htmltable(table_row(cells),"border=2 width=100%")
+    HTML+="</body>"
+    return "<html>"+HTML+"</html>"
+
+#dog details
 @app.route("/<dogname>")
 def showlist(dogname):
-    return "list of images for " + dogname+ "<p><ul><li><a href=\"/images/nugget1.jpg\">Nuggets birthday</a></li></ul>"
+    conn = sqlite3.connect('finalproject.db')
+    c = conn.cursor()
+    HTML=""
+    SQL="SELECT dog,descr,filename FROM pictures where dog='"+dogname+"'"
+    #return SQL
+    for row in c.execute(SQL):
+        descr=row[1]
+        filename=row[2]
+        HTML+= "<li><a href=\"/images/"+filename+"\">"+descr+"</a>"
 
+    if HTML=="": 
+        HTML="no pictures of "+dogname+" found"
+    else:
+        HTML="Pictures of "+dogname+":<br>"+HTML 
+
+    return HTML
+
+
+#display image
 @app.route("/images/<filename>")
 def showimages(filename):
     return send_file( os.path.join("image_folder",filename), mimetype='image/gif')
